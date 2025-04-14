c045555506c028b69a24d2ea946fcdc98c750528
Kyrillos Kamal <kyrilloskamal2025@gmail.com>
2025-04-14 15:18:36 +0200

    Add files via upload

---
+++ Dart Tutorial/1- First_Lesson/lib/main.dart
@@ -0,0 +1,23 @@
+/* -------------------------------------------------------------------------- */
+/*                                  Variables                                 */
+/* -------------------------------------------------------------------------- */
+
+/**
+  * var name = "Ahmed"; // Dart بتفهم النوع تلقائي
+  * String city = "Cairo";
+  * int age = 25;
+  * double height = 1.75;
+  * bool isActive = true;
+ **/
+void main() {
+  var name = "Ahmed"; // Dart بتفهم النوع تلقائي
+  String city = "Cairo";
+  int age = 25;
+  double height = 1.75;
+  bool isActive = true;
+  print(name);
+  print(age);
+  print(city);
+  print(height);
+  print(isActive);
+}
